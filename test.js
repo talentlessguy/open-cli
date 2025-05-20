950d6959839b2143ac4bc25295ea709e7e3e0af8
v1rtl <hi@v1rtl.site>
2025-05-20 15:39:10 +0300

    Add a test for stdin case

--- test.js
+++ test.js
@@ -1,7 +1,21 @@
+import {createReadStream} from 'node:fs';
+import path from 'node:path';
 import test from 'ava';
 import {execa} from 'execa';
 
 test('main', async t => {
 	const {stdout} = await execa('./cli.js', ['--version']);
 	t.true(stdout.length > 0);
+});
+
+test('supports opening files from stdin', async t => {
+	const cliPath = path.join(import.meta.dirname, './cli.js');
+
+	const stdinStream = createReadStream(cliPath);
+
+	const subprocess = execa(cliPath, {
+		input: stdinStream,
+	});
+
+	await t.notThrowsAsync(subprocess);
 });
